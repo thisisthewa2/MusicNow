9d1b086a278f4aa195f7fa9ae8436f33d701b270
thisisthewa2 <thisisthewa2.gmail.com>
2023-03-10 18:15:09 +0900

    Initial Commit

---
+++ MusicNow/MusicNowApp.swift
@@ -0,0 +1,17 @@
+//
+//  MusicNowApp.swift
+//  MusicNow
+//
+//  Created by 안윤진 on 2023/03/10.
+//
+
+import SwiftUI
+
+@main
+struct MusicNowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
